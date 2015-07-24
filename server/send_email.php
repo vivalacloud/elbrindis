4e1b25297a99514017f25e2f47f1db0afe5174cd
MiGatoSeneca <pablo@vivalacloud.com>
2015-07-24 11:54:02 +0200

    update

--- server/send_email.php
+++ server/send_email.php
@@ -5,14 +5,15 @@
   $user_subject="Elbrindisdelaño: ".$_POST["name"];
 
   $user_content="";
-  $user_content.="<h3 class=''>Invitación</h3>
-    <h4 class=''>".$_POST["name"]."</h4>
+  $user_content.="<h3 class=''>".$_POST["name"]."</h3>
+    <h4 class=''>".$_POST["email"]."</h4>
     <p class=''>".$_POST["comments"]."</p>";
 
   $mail_header="MIME-Version: 1.0\r\nContent-type: text/html\r\nFrom: no-reply@xn--elbrindisdelao-2nb.com";
   error_log($user_content);
 
-  mail("pablo@citious.com",$user_subject,$user_content,$mail_header);
+  mail("bustibri@gmail.com",$user_subject,$user_content,$mail_header);
+  mail($_POST["email"],$user_subject,$user_content,$mail_header);
   error_log("[Brindis][SendEmail] Email sent (pablo@citious.com)");
   $response["result"]=true;
   echo json_encode($response);
