499850c202c3d1eeb7482413b22be4b0e852bfda
MiGatoSeneca <pablo@vivalacloud.com>
2015-07-24 11:55:42 +0200

    update

--- server/send_email.php
+++ server/send_email.php
@@ -13,8 +13,9 @@
   error_log($user_content);
 
   mail("bustibri@gmail.com",$user_subject,$user_content,$mail_header);
+  error_log("[Brindis][SendEmail] Email sent (bustibri@gmail.com)");
   mail($_POST["email"],$user_subject,$user_content,$mail_header);
-  error_log("[Brindis][SendEmail] Email sent (pablo@citious.com)");
+  error_log("[Brindis][SendEmail] Email sent (".$_POST["email"].")");
   $response["result"]=true;
   echo json_encode($response);
   error_log("[Brindis][SendEmail] END");
