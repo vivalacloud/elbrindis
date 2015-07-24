b1c231a32e03300be9bfa96344f1362ceaaf2cdf
MiGatoSeneca <pablo@vivalacloud.com>
2015-07-24 11:49:01 +0200

    update

--- server/send_email.php
+++ server/send_email.php
@@ -5,12 +5,11 @@
   $user_subject="Elbrindisdelaño: ".$_POST["name"];
 
   $user_content="";
-  $user_content.="
-    <h3 class=''>Invitación</h3>
-    <h4 class=''>".$_POST["name"]."</h3>
+  $user_content.="<h3 class=''>Invitación</h3>
+    <h4 class=''>".$_POST["name"]."</h4>
     <p class=''>".$_POST["comments"]."</p>";
 
-  $mail_header="MIME-Version: 1.0\r\nContent-type: text/html\r\nFrom: no-reply@91.121.84.11";
+  $mail_header="MIME-Version: 1.0\r\nContent-type: text/html\r\nFrom: no-reply@xn--elbrindisdelao-2nb.com";
   error_log($user_content);
 
   mail("pablo@citious.com",$user_subject,$user_content,$mail_header);
